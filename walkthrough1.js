e789884045ff2822ec941bcdb8b4c092c4eb812c
JD.1010 1100 1110 <jzrl.ace.dc@gmail.com>
2025-03-14 12:02:34 -0400

    Add files via upload

---
+++ walkthrough1.js
@@ -0,0 +1,27 @@
+// comment
+// ctrl + u to view source code
+// display the source code of the file
+alert("Hello, World!");// display the alert message
+console.log("Hello, there!");// display the message in the console
+document.write("Hello, Welcome!");// display the message in the document
+prompt("What is your name?");// display the prompt message
+//declare a variable
+let name = "ABC";
+let age = 7;
+//document.write(age +1);
+const tax = 0.13;
+//array =list of fruits
+let fruits = ["apple", "banana", "orange"];
+//num
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//comparison operator with conditions
+//if/else {<, >, <=, >=, ==, !=}
+//logical operators &&, ||
+let validage = 78;
+if(validage !== 5 && validage >70 ){
+    document.write("You are old enough to drive");
+}else if(validage < 13){
+    document.write("You are too young to drive");
+}else{
+    document.write("You are eligible to drive");
+}
